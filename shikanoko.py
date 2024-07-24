7aeae864541df684c7844b26fd8c66b7fdc11f82
brrik0677 <leo.luvs.footwork@gmail.com>
2024-07-24 09:55:00 +0900

    bug fix

--- shikanoko.py
+++ shikanoko.py
@@ -4,7 +4,8 @@ str_list = []
 str_list.extend(strings)
 
 #最悪この回数見つからなかったらループ終了↓
-MAX_COUNT = 10
+#負の値にしたら無限に見つけるまでやる
+MAX_COUNT = -1
 
 main_dict = {}
 
@@ -24,7 +25,7 @@ for single_str in str_list:
 count = 0
 while True:
     output_list = ["し"]
-    for loops in range(len(strings)):
+    for loops in range(len(strings)-1):
         keys = output_list[-1]
         nexts = main_dict[keys]
         rnd = randint(0, len(nexts)-1)
@@ -37,11 +38,11 @@ while True:
 
         print(output_str)
         if output_str == strings:
-            print("しかのこ出た。")
+            print(count, "回目の挑戦でしかのこ出た。")
             break
         else:
             count += 1
         
-        if count>=MAX_COUNT:
+        if MAX_COUNT>0 and count>=MAX_COUNT:
             print(count, "回挑戦しましたがダメでした。")
             break
